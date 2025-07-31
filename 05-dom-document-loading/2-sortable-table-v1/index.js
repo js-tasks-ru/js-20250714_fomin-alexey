930409073abb9195cf8aa30f656793b10c35f2aa
Drongo <fomin.alexey@outlook.com>
2025-07-31 10:00:51 +0300

    выгрузил задание 5.2

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -2,5 +2,18 @@ export default class SortableTable {
   constructor(headerConfig = [], data = []) {
 
   }
+
+
+  createTemplate() {
+    return `
+
+    `;
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTemplate();
+    this.element = element.firstElementChild;
+  }
 }
 
